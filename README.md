b0e79e6160a376c05e69d78a8d3afa6a8cc8252a
BennyE <bennye_hh@posteo.net>
2022-07-31 21:51:57 +0200

    Add QR Code feature request

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # stellar-add-guest
-stellar-add-guest is a small tool to generate a new guest for Stellar Wireless (Enterprise mode) in OmniVista 2500 / Cirrus
+stellar-add-guest is a small tool to generate a new guest for Stellar Wireless (Enterprise mode) in OmniVista 2500 hosted on OmniSwitch with AOS Release 8.
+
+## Overview (meant to be executed on OmniSwitch with access to OmniVista 2500)
+![image](https://user-images.githubusercontent.com/5174414/150031492-3dbfc95d-4b2c-4882-a074-0547b2645a79.png)
+
+## Edit the settings-example.json and store it as settings.json
+```
+{
+    "ov_hostname": "hostname or IP address of OV e.g. omnivista.home but without https",
+    "ov_username": "admin",
+    "ov_password": "<your ov admin password>",
+    "validate_https_certificate": "no",
+    "send_emails": "yes",
+    "ssid_name": "Stellar-Captive",
+    "encryption": "WPA/WPA2 or OPEN",
+    "psk": "The PSK of the SSID, ignored if encryption is set to OPEN",
+    "print_cleartext_psk": "yes",
+    "send_psk_via_mail": "yes",
+    "runs_on_omniswitch": "yes",
+    "email_from": "<your from email address>",
+    "smtp_server": "smtp.gmail.com",
+    "smtp_auth": "yes",
+    "smtp_port": 587,
+    "smtp_user": "<your SMTP login name, often your email_from>",
+    "smtp_password": "<your SMTP password>",
+    "language": "de",
+    "email_to": "<your email_to, if no argv is given>",
+    "guest_prefix": "gast_",
+    "guest_duration_in_days": 31
+}
+```
+## Execute the Python script on the OmniSwitch
+```
+Router-> python3 /flash/python/stellar-add-guest.py <email-address@mailaddressdomain.com>
+[+] Reading settings.json file
+[!] Ignoring certificate warnings or self-signed certificates!
+[!] You should really fix this!
+[+] Updating email_to address to: <email-address@mailaddressdomain.com>
+[*] Attempting to connect to OmniVista server @ https://omnivista.home
+[*] Connection to omnivista.home successful!
+````
+## Language set to English (settings.json -> "language": "en") 
+![image](https://user-images.githubusercontent.com/5174414/150029499-7d9003d2-8e34-4867-880e-d698b7adffb0.png)
+
+## Language set to German (settings.json -> "language": "de") 
+![image](https://user-images.githubusercontent.com/5174414/150029836-2f4997ac-7808-4c60-9de1-73c1fa7c6931.png)
+
+## New functions in this branch/release
+
--- stellar-add-guest.py
+++ stellar-add-guest.py
@@ -29,15 +29,38 @@ import time
 import random
 import string
 # Not needed, thus saving some system resource
-#import uuid
+import uuid
 import datetime
+try:
+    import pyqrcode
+except ImportError as ie:
+    sys.exit("Please install pyqrcode & pypng!")
 
 #
 # Functions
 #
 
+def add_qr_code(language, content_id, print_cleartext_psk, psk):
+    if language == "de":
+        qr_text = f"""
+        <p>
+        Scannen Sie den folgenden QR-Code um Zugriff auf das Wireless LAN zu erhalten:</br>
+        <center><img src="cid:image3_{content_id}" style="max-height: 150px;"></img>
+        {'</br>Pre-Shared Key (PSK): ' + psk if print_cleartext_psk == "yes" else ""}</center>
+        </p>
+        """
+    else:
+        qr_text = f"""
+        <p>
+        Scan the following QR code to get access to the Wi-Fi network:</br>
+        <center><img src="cid:image3_{content_id}" style="max-height: 150px;"></img>
+        {'</br>Pre-Shared Key (PSK): ' + psk if print_cleartext_psk == "yes" else ""}</center>
+        </p>
+        """
+    return qr_text
+
 def send_mail(email_from, email_to, ssid_name, ga_username, ga_password, ga_valid_until, language,
-                smtp_server, smtp_auth, smtp_user, smtp_port, smtp_password):
+                smtp_server, smtp_auth, smtp_user, smtp_port, smtp_password, send_psk_via_mail, psk, print_cleartext_psk):
 
     # Send an HTML email with an embedded image and a plain text message for
     # email clients that don't want to display the HTML.
@@ -56,9 +79,9 @@ def send_mail(email_from, email_to, ssid_name, ga_username, ga_password, ga_vali
     # Create the root message and fill in the from, to, and subject headers
     msgRoot = MIMEMultipart('related')
     if language == "de":
-        msgRoot['Subject'] = "Neuer Gastzugang für Gäste-WLAN: {0}".format(ssid_name)
+        msgRoot['Subject'] = f"Neuer Gastzugang für Gäste-WLAN: {ssid_name}"
     else:
-        msgRoot['Subject'] = "New guest account for guest SSID: {0}".format(ssid_name)
+        msgRoot['Subject'] = f"New guest account for guest SSID: {ssid_name}"
     msgRoot['From'] = strFrom
     msgRoot['To'] = strTo
     msgRoot.preamble = 'This is a multi-part message in MIME format.'
@@ -69,34 +92,36 @@ def send_mail(email_from, email_to, ssid_name, ga_username, ga_password, ga_vali
     msgRoot.attach(msgAlternative)
 
     # Generate an UUID for uniqe attachment Content-IDs
-    # We don't need this for this module and will save some system resources
-    content_id = "abcdefg"
+    # Re-introduced for QR code functionality
+    content_id = uuid.uuid1().hex
 
     if language == "de":
-        msgText = MIMEText("""
+        msgText = MIMEText(f"""
 Hallo,
-soeben wurde ein neuer Gastzugang für das WLAN \"{0}\" erstellt.
-Benutzername: {1}
-Passwort: {2}
-Gültig bis: {3}
+soeben wurde ein neuer Gastzugang für das WLAN \"{ssid_name}\" erstellt.
+Benutzername: {ga_username}
+Passwort: {ga_password}
+Gültig bis: {ga_valid_until}
+{'Pre-Shared Key (PSK): ' + psk if print_cleartext_psk == "yes" else ""}
 Bis bald!
-        """.format(ssid_name, ga_username, ga_password, ga_valid_until))
+        """)
     else:
-        msgText = MIMEText("""
+        msgText = MIMEText(f"""
 Hi,
-a new guest account for SSID \"{0}\" was just created.
-Username: {1}
-Password: {2}
-Valid until: {3}
+a new guest account for SSID \"{ssid_name}\" was just created.
+Username: {ga_username}
+Password: {ga_password}
+Valid until: {ga_valid_until}
+{'Pre-Shared Key (PSK): ' + psk if print_cleartext_psk == "yes" else ""}
 Thanks,
 Regards,
 The ALE Stellar Wireless Team
-        """.format(ssid_name, ga_username, ga_password, ga_valid_until))
+        """)
 
     msgAlternative.attach(msgText)
 
     if language == "de":
-        mail_content = """
+        mail_content = f"""
 <!DOCTYPE html>
 <html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <style>
@@ -217,14 +242,23 @@ margin-left: 15px;
 <p></p>
 <ul>
 <li>
-<p><a href="https://www.al-enterprise.com/"><img src="cid:image1_{4}" height="60px"></a>
-<a href="https://www.al-enterprise.com/"><img src="cid:image2_{4}" height="75px"></a>
-</p>
+<table>
+<tr>
+<td>
+<a href="https://www.al-enterprise.com/"><img src="cid:image1_{content_id}" style="max-height: 60px;"></img></a>
+</td>
+<td>&nbsp;</td>
+<td>
+<a href="https://www.al-enterprise.com/"><img src="cid:image2_{content_id}" style="max-height: 75px;"></img></a>
+</td>
+</tr>
+</table>
 <p>Hallo,</p>
-<p>soeben wurde ein neuer Gastzugang für das WLAN \"{0}\" erstellt.</p>
-<p>Benutzername: <b>{1}</b><br/>
-Passwort: <b>{2}</b><br/>
-Gültig bis: <b>{3}</b></p>
+<p>soeben wurde ein neuer Gastzugang für das WLAN \"{ssid_name}\" erstellt.</p>
+<p>Benutzername: <b>{ga_username}</b><br/>
+Passwort: <b>{ga_password}</b><br/>
+Gültig bis: <b>{ga_valid_until}</b></p>
+{add_qr_code(language, content_id, print_cleartext_psk, psk) if send_psk_via_mail == "yes" else ""}
 <p>
 Danke und Gru&szlig;,<br>
 Ihr ALE Stellar Wireless Team
@@ -232,9 +266,9 @@ Ihr ALE Stellar Wireless Team
 </li>
 </ul>
 </body></html>
-    """.format(ssid_name, ga_username, ga_password, ga_valid_until, content_id)
+    """
     else:
-        mail_content = """
+        mail_content = f"""
 <!DOCTYPE html>
 <html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <style>
@@ -355,14 +389,23 @@ margin-left: 15px;
 <p></p>
 <ul>
 <li>
-<p><a href="https://www.al-enterprise.com/"><img src="cid:image1_{4}" height="60px"></a>
-<a href="https://www.al-enterprise.com/"><img src="cid:image2_{4}" height="75px"></a>
-</p>
+<table>
+<tr>
+<td>
+<a href="https://www.al-enterprise.com/"><img src="cid:image1_{content_id}" style="max-height: 60px;"></img></a>
+</td>
+<td>&nbsp;</td>
+<td>
+<a href="https://www.al-enterprise.com/"><img src="cid:image2_{content_id}" style="max-height: 75px;"></img></a>
+</td>
+</tr>
+</table>
 <p>Hi,</p>
-<p>a new guest account for SSID \"{0}\" was just created.</p>
-<p>Username: <b>{1}</b><br/>
-Password: <b>{2}</b><br/>
-Valid until: <b>{3}</b></p>
+<p>a new guest account for SSID \"{ssid_name}\" was just created.</p>
+<p>Username: <b>{ga_username}</b><br/>
+Password: <b>{ga_password}</b><br/>
+Valid until: <b>{ga_valid_until}</b></p>
+{add_qr_code(language, content_id, print_cleartext_psk, psk) if send_psk_via_mail == "yes" else ""}
 <p>
 Thanks,
 Regards,<br>
@@ -371,33 +414,58 @@ The ALE Stellar Wireless Team
 </li>
 </ul>
 </body></html>
-    """.format(ssid_name, ga_username, ga_password, ga_valid_until, content_id)
+    """
 
     # We reference the image in the IMG SRC attribute by the ID we give it below
     msgText = MIMEText(mail_content, 'html')
     msgAlternative.attach(msgText)
 
     # ALE Logo
-    fp = open('/flash/python/logos/al_enterprise_bk_50mm.png', 'rb')
-#    fp = open('logos/al_enterprise_bk_50mm.png', 'rb')
+    if runs_on_omniswitch == "yes":
+        fp = open('/flash/python/logos/al_enterprise_bk_50mm.png', 'rb')
+    else:
+        fp = open('logos/al_enterprise_bk_50mm.png', 'rb')
     msgImage = MIMEImage(fp.read())
     fp.close()
 
     # Define the image's ID as referenced above
     # Avoid that the mail client can cache a previous QR code by giving a custom name
-    msgImage.add_header('Content-ID', '<image1_{0}>'.format(content_id))
+    msgImage.add_header('Content-ID', f'<image1_{content_id}>')
     msgRoot.attach(msgImage)
 
     # Stellar Logo
-    fp = open('/flash/python/logos/stellar-logo.png', 'rb')
-#    fp = open('logos/stellar-logo.png', 'rb')
+    if runs_on_omniswitch == "yes":
+        fp = open('/flash/python/logos/stellar-logo.png', 'rb')
+    else:
+        fp = open('logos/stellar-logo.png', 'rb')
     msgImage = MIMEImage(fp.read())
     fp.close()
 
     # Define the image's ID as referenced above
     # Avoid that the mail client can cache a previous QR code by giving a custom name
-    msgImage.add_header('Content-ID', '<image2_{0}>'.format(content_id))
+    msgImage.add_header('Content-ID', f'<image2_{content_id}>')
     msgRoot.attach(msgImage)
+
+    # Add QR Code
+    if send_psk_via_mail == "yes":
+        # Generate a QR code to login to this SSID
+        # QR format: "WIFI:T:WPA;S:SSID;P:PSK;;"
+        if encryption != "OPEN":
+            pskqr = pyqrcode.create(f"WIFI:T:{encryption};S:{ssid_name};P:{psk};;")
+        else:
+            pskqr = pyqrcode.create(f"WIFI:T:nopass;S:{ssid_name};P:;;")
+        if runs_on_omniswitch == "yes":
+            pskqr.png("/flash/python/logos/qrcode.png", scale=8)
+            fp = open('/flash/python/logos/qrcode.png', 'rb')
+            msgImage = MIMEImage(fp.read())
+            fp.close()
+        else:
+            pskqr.png("logos/qrcode.png", scale=8)
+            fp = open('logos/qrcode.png', 'rb')
+            msgImage = MIMEImage(fp.read())
+            fp.close()
+        msgImage.add_header('Content-ID', f'<image3_{content_id}>')
+        msgRoot.attach(msgImage)
 
     # Send the email
     import smtplib
@@ -419,8 +487,8 @@ if __name__ == "__main__":
     print("[+] Reading settings.json file")
     try:
 # Depending on the target platform to run/host this script you may need to modify this
-        with open("/flash/python/settings.json", "r") as json_data:
-#        with open("settings.json", "r") as json_data:
+#        with open("/flash/python/settings.json", "r") as json_data:
+        with open("settings.json", "r") as json_data:
             settings = json.load(json_data)
             ov_hostname = settings["ov_hostname"]
             ov_username = settings["ov_username"]
@@ -429,6 +497,11 @@ if __name__ == "__main__":
             email_from = settings["email_from"]
             send_emails = settings["send_emails"]
             ssid_name = settings["ssid_name"]
+            encryption = settings["encryption"]
+            psk = settings["psk"]
+            print_cleartext_psk = settings["print_cleartext_psk"]
+            send_psk_via_mail = settings["send_psk_via_mail"]
+            runs_on_omniswitch = settings["runs_on_omniswitch"]
             smtp_server = settings["smtp_server"]
             smtp_auth = settings["smtp_auth"]
             smtp_user = settings["smtp_user"]
@@ -474,11 +547,11 @@ if __name__ == "__main__":
 
     # We support to send the guest_account details via email to the account creator
     if len(sys.argv) == 2:
-        print("[+] Updating email_to address to: {0}".format(sys.argv[1]))
+        print(f"[+] Updating email_to address to: {sys.argv[1]}")
         email_to = sys.argv[1]
 
     # Test connection to OmniVista
-    print("[*] Attempting to connect to OmniVista server @ https://{0}".format(ov_hostname))
+    print(f"[*] Attempting to connect to OmniVista server @ https://{ov_hostname}")
 
     req = requests.Session()
 
@@ -488,15 +561,15 @@ if __name__ == "__main__":
 
     # Check if we die on the HTTPS certificate
     try:
-        ov = req.get("https://{0}".format(ov_hostname), verify=check_certs)
+        ov = req.get(f"https://{ov_hostname}", verify=check_certs)
     except requests.exceptions.SSLError as sslerror:
         print(sslerror)
         sys.exit("[!] Caught issues on certificate, try to change \"validate_https_certificate\" to \"no\" in settings.json. Exiting!")
 
     if ov.status_code == 200:
-        print("[*] Connection to {0} successful!".format(ov_hostname))
+        print(f"[*] Connection to {ov_hostname} successful!")
     else:
-        sys.exit("[!] Connection to {0} failed, exiting!".format(ov_hostname))
+        sys.exit(f"[!] Connection to {ov_hostname} failed, exiting!")
 
     ov_login_data = {"userName" : ov_username, "password" : ov_password}
     ov_header = {"Content-Type": "application/json"}
@@ -504,20 +577,27 @@ if __name__ == "__main__":
     # requests.post with json=payload was introduced in version 2.4.2
     # otherwise it would need to be "data=json.dumps(ov_login_data),"
 
-    ov = req.post("https://{0}/rest-api/login".format(ov_hostname),
+    ov = req.post(f"https://{ov_hostname}/rest-api/login",
                 headers=ov_header,
                 json=ov_login_data,
                 verify=check_certs)
 
     if ov.status_code == 200:
-        token = ov.json()
-        token = token["accessToken"]
-        ov_header["Authorization"] = "Bearer {0}".format(token)
+        ov_header["Authorization"] = f"Bearer {ov.json()['accessToken']}"
     else:
         sys.exit("[!] The connection to OmniVista was not successful! Exiting!")
     letters = string.ascii_lowercase
-    # keep in mind that the minimum password length is 6!
-    random_username = ''.join(random.choice(letters) for i in range(6))
+    random_username = ''.join(random.choice(letters) for _ in range(6))
+
+    # You may need to adapt this to your local language
+    undesireable_words = ["cunt", "pussy", "nigger", "penis", "fotze", "hitler", "fuck"]
+
+    for uw in undesireable_words:
+        if uw in random_username:
+            print("[!] Detected undesirable word in username, generating a new one!")
+            # I assume our chances are very low to generate two undesireable words in a row
+            random_username = ''.join(random.choice(letters) for _ in range(6))
+            break
 
     # Doesn't contain "l", "I", "O", "0" and "1" on purpose to avoid mistyping, thx Michael
     letters = "ABCDEFGHJKMNPQRSTUVWXYZ23456789abcdefghijkmnopqrstuvwxyz"
@@ -530,13 +610,20 @@ if __name__ == "__main__":
     # Generate a secure random password for Guest Accounts
     random_password = "".join(r.choice(letters) for _ in range(8))
 
+    for uw in undesireable_words:
+        if uw in random_password.lower():
+            print("[!] Detected undesirable word in password, generating a new one!")
+            # I assume our chances are very low to generate two undesireable words in a row
+            random_password = "".join(r.choice(letters) for _ in range(8))
+            break
+
     # Note that JAVA (on OV side in the backend) uses Milliseconds for epoch
     add_guest_data = {"creator":ov_username,
     "accountType":"Account",
     "description":"",
-    "password":"{0}".format(random_password),
-    "repeat":"{0}".format(random_password),
-    "username":"{0}{1}".format(guest_prefix, random_username),
+    "password":f"{random_password}",
+    "repeat":f"{random_password}",
+    "username":f"{guest_prefix}{random_username}",
     "dataQuotaAmount":1000,
     "dateOfEffective":int(round(time.time() * 1000)),
     "accountValidityPeriod":int(round((time.time() + (86400 * guest_duration_in_days)) * 1000)),
@@ -544,18 +631,18 @@ if __name__ == "__main__":
 
     # Document who created the account
     if language == "de":
-        add_guest_data["description"] = "Gastzugang wurde durch {0} erstellt".format(email_to) if len(sys.argv) == 2 else "Gastzugang wurde durch {0} erstellt".format(ov_username)
+        add_guest_data["description"] = f"Gastzugang wurde durch {email_to} erstellt" if len(sys.argv) == 2 else f"Gastzugang wurde durch {ov_username} erstellt"
     else:
-        add_guest_data["description"] = "Guest account was created by {0}".format(email_to) if len(sys.argv) == 2 else "Guest account was created by {0}".format(ov_username)
+        add_guest_data["description"] = f"Guest account was created by {email_to}" if len(sys.argv) == 2 else f"Guest account was created by {ov_username}"
 
     # Create the guest account
-    add_guest_resp = req.post("https://{0}/api/ham/guest/account/addAccount".format(ov_hostname), headers=ov_header, json=add_guest_data, verify=check_certs)
+    add_guest_resp = req.post(f"https://{ov_hostname}/api/ham/guest/account/addAccount", headers=ov_header, json=add_guest_data, verify=check_certs)
 
     if add_guest_resp.json()["errorCode"] != 0:
-        sys.exit("Guest account creation failed with: {0}".format(add_guest_resp.json()["errorMessage"]))
+        sys.exit(f"Guest account creation failed with: {add_guest_resp.json()['errorMessage']}")
 
     # Logout from API
-    ov3 = req.get("https://{0}/rest-api/logout".format(ov_hostname), verify=check_certs)
+    ov3 = req.get(f"https://{ov_hostname}/rest-api/logout", verify=check_certs)
 
     # Generate how long the Guest Account is valid
     if language == "de":
@@ -564,4 +651,5 @@ if __name__ == "__main__":
         ga_valid_until = datetime.datetime.fromtimestamp(add_guest_data["accountValidityPeriod"] / 1000).strftime('%Y-%m-%d %H:%M:%S')
 
     if send_emails == "yes":
-        send_mail(email_from, email_to, ssid_name, guest_prefix + random_username, random_password, ga_valid_until, language, smtp_server, smtp_auth, smtp_user, smtp_port, smtp_password)
+        send_mail(email_from, email_to, ssid_name, guest_prefix + random_username, random_password, ga_valid_until, language, smtp_server,
+                    smtp_auth, smtp_user, smtp_port, smtp_password, send_psk_via_mail, psk, print_cleartext_psk)
